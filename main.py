41c577b8286f94992296fb561331856bd9115363
Mehdi <mehdipailot@hotmail.fr>
2023-11-03 11:26:43 +0100

    maj

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
+# Importer les bibliothèques nécessaires
 import requests
 from bs4 import BeautifulSoup
 import csv
+from pprint import pprint
 
-# URL de la page Web
+# URL de la page Web contenant les données des épisodes
 url = "https://www.spin-off.fr/calendrier_des_series.html"
 
-# Effectuer la requête HTTP et récupérer le contenu de la page
+# Effectuer une requête HTTP pour obtenir le contenu de la page
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Trouver la table contenant les données des épisodes
-table = soup.find("table", class_="padding2")
+# Trouver la table contenant les données des épisodes (la 8ème table dans ce cas)
+tables = soup.find_all("table", class_="padding2")
+if len(tables) >= 8:
+    target_table = tables[7]
 
 # Initialiser une liste pour stocker les données des épisodes
 episodes_data = []
 
 # Parcourir les lignes de la table et extraire les informations nécessaires
-for row in table.find_all("tr"):
-    cells = row.find_all("td", class_="nowrap left vmiddle")
-    if len(cells) > 1:
-        serie = cells[1].text.strip()
-        episode_info = row.find_all("td", class_="vmiddle")
-        episode_number = episode_info[0].text.strip()
-        season_number = episode_info[1].text.strip()
-        air_date = episode_info[2].text.strip()
-        country = episode_info[3].text.strip()
-        channel = episode_info[4].text.strip()
-        relative_url = episode_info[0].find("a")["href"]
-        episode_url = f"https://www.spin-off.fr/{relative_url}"
+for index, row in enumerate(target_table.find_all("tr"), 1):
+    if index == 1:
+        pass  # Ignorer la première ligne qui contient les en-têtes
+    else:
+        cells = row.find_all("td", class_="floatleftmobile td_jour")
 
-        # Ajouter les données de l'épisode à la liste
-        episodes_data.append((serie, episode_number, season_number, air_date, country, channel, episode_url))
+        for cell in cells:
+            # Extraire la date de l'épisode à partir de l'attribut 'id' du div_jour
+            div_jour = cell.find('div', class_='div_jour')
+            if div_jour:
+                episode_id = div_jour['id']
+                day = episode_id.split('jour_')[1]
 
-# Enregistrer les données dans un fichier CSV
-with open("data/files/episodes.csv", "w", newline="", encoding="utf-8") as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=";")
-    # Écrire l'en-tête du fichier CSV
-    csvwriter.writerow(["Série", "Numéro de l'épisode", "Numéro de la saison", "Date de diffusion", "Pays d'origine", "Chaîne", "URL de l'épisode"])
-    # Écrire les données des épisodes dans le fichier CSV
-    csvwriter.writerows(episodes_data)
+            # Extraire les détails de l'épisode à partir des balises <a> dans la cellule
+            for episode in cell.find_all('span', class_='calendrier_episodes'):
+                spanContent = episode.find_all('a')
+                episode_name = spanContent[0].text
+                episode_season = spanContent[1].text.split('.')[0]
+                episode_number = spanContent[1].text.split('.')[1]
+
+                # Extraire le nom du pays et de la chaîne à partir des balises <img> précédentes
+                channel = episode.find_previous('img')['alt']
+                country = episode.find_previous('img').find_previous('img')['alt']
+
+                # Ajouter les données de l'épisode à la liste episodes_data
+                episodes_data.append({
+                    'name': episode_name,
+                    'episode_number': episode_number,
+                    'episode_season': episode_season,
+                    'date': day,
+                    'country': country,
+                    'channel': channel
+                })
+
+# Écrire les données dans un fichier CSV
+with open('data/files/episodes.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['name', 'episode_number', 'episode_season', 'date', 'country', 'channel']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(episodes_data)
+
+# Afficher les données extraites
+pprint(episodes_data)
