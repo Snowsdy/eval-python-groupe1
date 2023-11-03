679b9ff0a3a8182bdef9ebe58eca82f364755f99
TeyKz <81241850+TeyKz@users.noreply.github.com>
2023-11-03 15:59:11 +0100

    Modif plus d'épisodes + readme

--- README.md
+++ README.md
@@ -10,21 +10,30 @@ pip install -r requirements.txt
 
 ```
 
-## Réponses aux mots qui reviennent le plus souvent dans les séries
+### Trois chaînes qui ont diffusé le plus d'épisodes :
+    Netflix: 471 épisodes
+    TF1: 158 épisodes
+    Disney+: 118 épisodes
+    
+### Trois pays qui ont diffusé le plus d'épisodes :
+    Etats-Unis: 1439 épisodes
+    France: 423 épisodes
+    Canada: 288 épisodes
 
-Mots les plus fréquents dans les noms des séries :
-'the': 23 occurrences
-'of': 7 occurrences
-'de': 3 occurrences
-'american': 3 occurrences
-'les': 3 occurrences
-'(2023)': 3 occurrences
-'all': 2 occurrences
-'horror': 2 occurrences
-'la': 2 occurrences
-'park': 2 occurrences
-'in': 2 occurrences
-'good': 2 occurrences
-'(2022)': 2 occurrences
-'soko': 2 occurrences
-'à': 2 occurrences
+### Mots les plus fréquents dans les noms des séries :
+- 'the': 23 occurrences
+- 'of': 7 occurrences
+- 'de': 3 occurrences
+- 'american': 3 occurrences
+- 'les': 3 occurrences
+- '(2023)': 3 occurrences
+- 'all': 2 occurrences
+- 'horror': 2 occurrences
+- 'la': 2 occurrences
+- 'park': 2 occurrences
+- 'in': 2 occurrences
+- 'good': 2 occurrences
+- '(2022)': 2 occurrences
+- 'soko': 2 occurrences
+- 'à': 2 occurrences
+
---
+++ bdd.py
@@ -0,0 +1,37 @@
+import csv
+import pandas as pd
+from sql import create_tables, add_new_episode
+
+# Créer les tables dans la base de données SQLite
+create_tables()
+
+with open('data/files/episodes.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)  # Ignorer l'en-tête
+    for row in csvreader:
+        name, episode_number, episode_season, date, country, channel, url = row
+        
+        # Vérification pour gérer les valeurs non numériques 'XX'
+        if episode_number.isdigit():
+            episode_number = int(episode_number)
+        else:
+            episode_number = None  # Ou une valeur par défaut si nécessaire
+        
+        if episode_season.isdigit():
+            episode_season = int(episode_season)
+        else:
+            episode_season = None  # Ou une valeur par défaut si nécessaire
+        
+        new_episode = {
+            'name': name,
+            'episode_number': episode_number,
+            'episode_season': episode_season,
+            'date': date,
+            'country': country,
+            'channel': channel,
+            'url': url
+        }
+        episode_df = pd.DataFrame([new_episode])
+        add_new_episode(episode_df)
+
+print("Les données ont été insérées dans la base de données.")
