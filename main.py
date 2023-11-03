83e6edf7a21895b9a8a30f2082caaba249f124c3
Mehdi <mehdipailot@hotmail.fr>
2023-11-03 11:56:43 +0100

    maj

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ for index, row in enumerate(target_table.find_all("tr"), 1):
                 episode_name = spanContent[0].text
                 episode_season = spanContent[1].text.split('.')[0]
                 episode_number = spanContent[1].text.split('.')[1]
+                
+                # Extraire l'URL relative de la page de l'épisode
+                episode_url = spanContent[0]['href']
 
                 # Extraire le nom du pays et de la chaîne à partir des balises <img> précédentes
                 channel = episode.find_previous('img')['alt']
@@ -51,12 +54,13 @@ for index, row in enumerate(target_table.find_all("tr"), 1):
                     'episode_season': episode_season,
                     'date': day,
                     'country': country,
-                    'channel': channel
+                    'channel': channel,
+                    'episode_url': episode_url  # Ajouter l'URL relative de la page de l'épisode
                 })
 
 # Écrire les données dans un fichier CSV
 with open('data/files/episodes.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['name', 'episode_number', 'episode_season', 'date', 'country', 'channel']
+    fieldnames = ['name', 'episode_number', 'episode_season', 'date', 'country', 'channel', 'episode_url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(episodes_data)
